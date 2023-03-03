94ced6a4ac03a3969721207a0bd7f8de692a2492
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-03-03 14:21:20 +0300

    ,

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,13 +1,9 @@
 package hiber.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
-@Component
 public class User {
 
     @Id
@@ -28,7 +24,6 @@ public class User {
 
     public User() {
     }
-    @Autowired
     public User(String firstName, String lastName, String email, Car car) {
         this.firstName = firstName;
         this.lastName = lastName;
